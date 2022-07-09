21156b29e1102a96d21d25f7f29b90df9ae0172b
Will Breiler <will.breiler@gmail.com>
2022-07-08 21:55:23 -0500

    Updated stuffs

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Will Breiler`,
+    siteTitleAlt: `Will Breiler - Personal Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteUrl: `https://wbreiler.me`,
+    siteDescription: `Personal portfolio for Will Breiler`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@wbreiler`,
   },
   plugins: [
     {
